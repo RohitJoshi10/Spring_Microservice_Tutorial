d435d5acedf5eaffd9e29827f3bd09a62dbc2cca
Rohit <rohit11joshi10@gmail.com>
2026-08-07 13:11:39 +0530

    Implemented Service Discovery Client for USER-SERVICE

--- userService/src/main/java/com/example/user/service/UserServiceApplication.java
+++ userService/src/main/java/com/example/user/service/UserServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient: In modern Spring Cloud versions, you no longer need any annotation on your main class to enable Eureka client functionality.
+// If you prefer to explicitly declare service discovery in your code, replace @EnableEurekaClient with the generic @EnableDiscoveryClient annotation.
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
